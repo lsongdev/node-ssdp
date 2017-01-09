4259ccb68ff49cd3ec80633d623a1f41eb46f04e
Lsong <song940@gmail.com>
2017-01-09 17:25:42 +0800

    modify

--- index.js
+++ index.js
@@ -1,98 +1,22 @@
-const udp          = require('dgram');
-const util         = require('util');
-const EventEmitter = require('events');
+const udp = require('dgram');
 
-/**
- * [Discovery description]
- */
 function Discovery(options){
-  if(!(this instanceof Discovery)){
-    return new Discovery(options);
-  }
-  EventEmitter.call(this);
-  var defaults = {
-    port     : 1900,
-    multicast: '239.255.255.250'
-  };
-  for(var k in options){
-    defaults[ k ] = options[ k ];
-  }
-  this.options = defaults;
   this.socket = udp.createSocket('udp4');
-  this.socket.on('message', this.process.bind(this));
-  return this;
+  this.socket.on('message', function(message, address) {
+    console.log(message, address);
+  }.bind(this));
 };
 
-util.inherits(Discovery, EventEmitter);
-
-Discovery.prototype.process = function(message, rinfo){
-  console.log(message, rinfo);
-  return this;
-};
-
-/**
- * [escape description]
- * @param  {[type]} str [description]
- * @return {[type]}     [description]
- */
-Discovery.escape = function(str){
-  if(/[:|\s]/.test(str)) 
-    return '"' + str + '"';
-  return str;
-};
-/**
- * [search description]
- * @param  {[type]} path    [description]
- * @param  {[type]} headers [description]
- * @return {[type]}         [description]
- */
-Discovery.prototype.search = function(path, headers){
-  var method = 'M-SEARCH';
-  path = path || '*';
-  headers = headers || {};
-  return this.send(method, path, headers);
-}
-/**
- * [send description]
- * @param  {[type]} method  [description]
- * @param  {[type]} path    [description]
- * @param  {[type]} headers [description]
- * @return {[type]}         [description]
- */
-Discovery.prototype.send = function(method, path, headers){
-  var request = [
-    [ method, path, 'HTTP/1.1' ].join(' ')
-  ];
-  Object.keys(headers).forEach(name => {
-    request.push([ name, Discovery.escape(headers[ name ]) ].join(': '));
-  });
-  request.push();
-  var message = new Buffer(request.join('\r\n'));
-  this.socket.send(message, 0, message.length,
-    this.options.port, this.options.multicast, function(){});
-  return this;
-};
-/**
- * [listen description]
- * @param  {[type]}   port     [description]
- * @param  {Function} callback [description]
- * @return {[type]}            [description]
- */
 Discovery.prototype.listen = function(port, callback){
-  if(typeof port === 'function'){
-    callback = port; port = null;
-  }
-  var err = null;
-  port = port || this.options.port;
-  try{
-    this.socket.bind(port, function(err){
-      this.setBroadcast(true);
-    });
-  }catch(e){
-    err = e;
-  }
-  callback && callback(err);
-  return this;
+  this.socket.bind(port, function(err) {
+    this.setBroadcast(true);
+    callback && callback(err, port);
+  });
+};
+
+Discovery.prototype.search = function(path, headers, callback) {
+  var buffer = new Buffer('M-SEARCH * HTTP/1.1\r\nMAN: \"ssdp:discover\"\r\nST: wifi_bulb\r\n');
+  this.socket.send(buffer, 0, buffer.length, 1982, '239.255.255.250');
 };
 
 module.exports = Discovery;
